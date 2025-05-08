5fc9a32010ade84dfc8d80e53c28a341a93c5b20
Matthias Nicola <matthias.nicola@snowflake.com>
2025-05-08 17:10:29 +0200

    Update STEP2_setup_primary_account.sql

--- sql/STEP2_setup_primary_account.sql
+++ sql/STEP2_setup_primary_account.sql
@@ -1,5 +1,5 @@
 -- Run this code in your PRIMARY Account
--- Make sure you update the four variables below (email_var, firstname_var and lastname_var)
+-- Make sure you update the four variables below: email_var, firstname_var, lastname_var, pwd_var
 
 USE ROLE accountadmin;
 
@@ -11,22 +11,36 @@ set lastname_var  = 'FILL_IN_YOUR_LAST_NAME';
 -- Use the same password for users in all accounts
 set pwd_var = 'FILL_IN_YOUR_PASSWORD';
 
-CREATE OR REPLACE WAREHOUSE compute_wh WAREHOUSE_SIZE=small INITIALLY_SUSPENDED=TRUE;
+CREATE OR REPLACE WAREHOUSE compute_wh WAREHOUSE_SIZE=xsmall INITIALLY_SUSPENDED=TRUE;
 GRANT ALL ON WAREHOUSE compute_wh TO ROLE public;
 
+--  Create a user and role for the sales domain:
+USE ROLE accountadmin;
 CREATE OR REPLACE ROLE sales_data_scientist_role;
-GRANT CREATE SHARE ON ACCOUNT                    TO ROLE sales_data_scientist_role;
-GRANT CREATE ORGANIZATION LISTING ON ACCOUNT     TO ROLE sales_data_scientist_role;
-GRANT MANAGE LISTING AUTO FULFILLMENT ON ACCOUNT TO ROLE sales_data_scientist_role;
-GRANT ROLE sales_data_scientist_role TO USER sales_admin;
 
 SET my_user_var = CURRENT_USER();
 ALTER USER identifier($my_user_var) SET DEFAULT_ROLE = sales_data_scientist_role;
 
+CREATE OR REPLACE USER sales_admin
+  PASSWORD = $pwd_var
+  LOGIN_NAME = sales_admin
+  DISPLAY_NAME = sales_admin
+  FIRST_NAME = $firstname_var
+  LAST_NAME = $lastname_var
+  EMAIL = $email_var
+  MUST_CHANGE_PASSWORD = FALSE
+  DEFAULT_WAREHOUSE = compute_wh
+  DEFAULT_ROLE = sales_data_scientist_role
+  COMMENT = 'Sales domain admin';
 
--- Next, create a user and role for the marketing domain
--- in the primary account:
+GRANT ROLE sales_data_scientist_role TO USER sales_admin;
+GRANT ROLE accountadmin              TO USER sales_admin;  -- for simplicity in this lab
+GRANT CREATE SHARE ON ACCOUNT                    TO ROLE sales_data_scientist_role;
+GRANT CREATE ORGANIZATION LISTING ON ACCOUNT     TO ROLE sales_data_scientist_role;
+GRANT MANAGE LISTING AUTO FULFILLMENT ON ACCOUNT TO ROLE sales_data_scientist_role;
 
+
+-- Next, create a user and role for the marketing domain:
 USE ROLE accountadmin;
 CREATE OR REPLACE ROLE marketing_analyst_role;
 
@@ -43,10 +57,12 @@ CREATE OR REPLACE USER marketing_admin
   COMMENT = 'Marketing domain admin';
 
 GRANT ROLE marketing_analyst_role TO USER marketing_admin;
-GRANT CREATE SHARE ON ACCOUNT TO ROLE marketing_analyst_role;
-GRANT CREATE ORGANIZATION LISTING ON ACCOUNT TO ROLE marketing_analyst_role;
+GRANT CREATE SHARE ON ACCOUNT                    TO ROLE marketing_analyst_role;
+GRANT CREATE ORGANIZATION LISTING ON ACCOUNT     TO ROLE marketing_analyst_role;
+GRANT MANAGE LISTING AUTO FULFILLMENT ON ACCOUNT TO ROLE sales_data_scientist_role;
 
--- Run this code in your PRIMARY account
+
+-- Continue to run this code in your PRIMARY account
 -- Create a secondary account in the same region (default!):
 USE ROLE orgadmin;
 
@@ -69,8 +85,7 @@ CREATE ORGANIZATION ACCOUNT hol_org_account
   must_change_password = false
   edition = enterprise; 
 
--- Make a note of your account names, URLs, and passwords! 
 -- Get an overview of all the accounts in the organization.
--- This SHOW command below should return 3 rows:
+-- This SHOW command should return 3 rows:
 
 SHOW ACCOUNTS;
